dc613a82a19ca0c50dbe1f9be52804c517176531
Shmakov Igor <shmakoff.work@gmail.com>
2025-11-04 20:28:16 +0000

    Update

--- Package.swift
+++ Package.swift
@@ -1,6 +1,4 @@
 // swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
@@ -9,32 +7,32 @@ let package = Package(
         .iOS("13.0")
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "TronWebSwift",
-            targets: ["TronWebSwift"]),
+        .library(name: "TronWebSwift", targets: ["TronWebSwift"]),
+        .library(name: "WalletCoreSwiftProtobuf", targets: ["WalletCoreSwiftProtobuf"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
          .package(url: "https://github.com/attaswift/BigInt.git", .upToNextMinor(from: "5.4.0")),
-         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.26.0"),
          .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.4.2"),
          .package(url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TronWebSwift",
             dependencies: [
                 "BigInt",
-                .product(name: "SwiftProtobuf", package: "swift-protobuf"),
                 "CryptoSwift",
-                .product(name: "secp256k1", package: "secp256k1.swift")
+                "WalletCoreSwiftProtobuf",
+                .product(name: "secp256k1", package: "secp256k1.swift"),
             ]
+        ),
+        .binaryTarget(
+            name: "WalletCoreSwiftProtobuf",
+            url: "https://github.com/trustwallet/wallet-core/releases/download/4.2.9/SwiftProtobuf.xcframework.zip",
+            checksum: "946efd4b0132b92208335902e0b65e0aba2d11b9dd6f6d79cc8318e2530c9ae0"
         ),
         .testTarget(
             name: "TronWebSwiftTests",
-            dependencies: ["TronWebSwift", "CryptoSwift"]),
+            dependencies: ["TronWebSwift", "CryptoSwift"]
+        ),
     ]
 )
