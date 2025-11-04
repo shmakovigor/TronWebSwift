ac329a460f34665709293ebda2e198e24aae1bcf
Shmakov Igor <shmakoff.work@gmail.com>
2025-11-04 23:50:19 +0000

    Update

--- Package.swift
+++ Package.swift
@@ -18,16 +18,13 @@ let package = Package(
     dependencies: [
          .package(url: "https://github.com/attaswift/BigInt.git", .upToNextMinor(from: "5.4.0")),
          .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.4.2"),
-         .package(url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.0")
+         .package(url: "https://github.com/gigabitcoin/secp256k1.swift.git", from: "0.6.0")
     ],
     targets: [
         .target(
             name: "TronWebSwift",
             dependencies: [
                 "BigInt",
-                // SwiftProtobuf should be provided by WalletCoreSwiftProtobuf when used with WalletCore
-                // If using TronWebSwift standalone, add: .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.26.0")
-                // and include: .product(name: "SwiftProtobuf", package: "swift-protobuf") in dependencies
                 "CryptoSwift",
                 .product(name: "secp256k1", package: "secp256k1.swift")
             ]
